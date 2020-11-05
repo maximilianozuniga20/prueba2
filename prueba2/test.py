22e839287010c1352760c1ebaa07995130c39db1
maximilianozuniga20 <73561300+maximilianozuniga20@users.noreply.github.com>
2020-11-04 22:20:43 -0300

    Integracion de pruebas unitarias

--- prueba2/test.py
+++ prueba2/test.py
@@ -1,11 +1,30 @@
+import unittest
 from django.test import TestCase
-from ..juego.models import Juegos
+from juego.models import Juegos
 
-class JuegoTestCase(TestCase):
- def setUp(self):
-  a1 = Juegos.objects.create(titulo="Ejemplo",descripcion="Esta es una prueba",precio="12000",plataforma="Prueba",imagen="core/demon.jpg")
-  Juegos.objects.create(titulo = "Harry Potter", autor = a1, resumen = "Resumen del libro")
+class PRUEBAUNITARIA(unittest.TestCase):
+    def test_crear_demo(self):
+        a = 1
+        self.assertEqual(a, 1)
+    
+    def test_crear_objeto(self):
+        juego = Juegos.objects.create(titulo="Ejemplo",
+                                      descripcion="Esta es una prueba",
+                                      precio="12000",
+                                      plataforma="Prueba",
+                                      imagen="core/demon.jpg")
+        juego.save()
+        self.assertTrue(juego,True)
 
- def test_juegos_titulo(self):
-  juego1 = Juegos.objects.get(titulo="Ejemplo")
-  self.assertEqual(juego1.titulo, "Esta es una prueba")  
+if __name__ == "__main__":
+    unittest.main()
+
+    
+
+        
+    
+    
+
+    
+
+
